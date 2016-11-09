baf599fad1b7a094c5f21cd340e8fadeae41aea7
Damian1234523 <dpistorius@posteo.de>
2016-11-09 18:02:08 +0100

    Installed devise gem, generated devise standard user model

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #email for devise needs real ip in production
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
