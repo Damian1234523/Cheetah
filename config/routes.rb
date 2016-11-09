54cc4b6693ef2c78f8bdf274c2cc9c130f9efe9e
Damian1234523 <dpistorius@posteo.de>
2016-11-09 21:09:36 +0100

    created routes and forced login on testhome

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'testhome#home'
+
   get 'testhome/home'
 
   get 'sessions/new'
-
   devise_for :users, controllers: { sessions: 'users/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
