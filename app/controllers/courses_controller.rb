f6f0a787b7a4889cde1026e69c1120a64bdbd831
Damian1234523 <dpistorius@posteo.de>
2016-11-23 09:30:51 +0100

    generated controllers for universities, courses, exams, lexturers

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,13 @@
+class CoursesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
---
+++ test/controllers/universities_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class UniversitiesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get universities_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get universities_create_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get universities_edit_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get universities_destroy_url
+    assert_response :success
+  end
+
+end
