d3a4ceaf5e350d2e7c70b7afaf83ae9645c8b8cc
Damian1234523 <dpistorius@posteo.de>
2016-11-23 15:44:43 +0100

    removed no_login layout from the seperate controllers implemented method layout_by_resource in applictaion_controller. It chooses the no_login layout if nescessary

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  #is used to give sites with no loginaccess another layout
+  layout :layout_by_resource
+  protected
+  def layout_by_resource
+    if devise_controller? && action_name == "new"
+      "no_login"
+    else
+      "application"
+    end
+  end
+
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,4 @@
 class Users::SessionsController < Devise::SessionsController
-  layout "no_login"
 # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
