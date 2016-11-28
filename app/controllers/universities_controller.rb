f7e30acbd27b3028e813bda3ebc92aae6860e13a
Damian1234523 <dpistorius@posteo.de>
2016-11-29 00:27:50 +0100

    fixed with ugly route still problems with arguments

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -1,10 +1,11 @@
 class UniversitiesController < ApplicationController
   before_action :authenticate_user!
   def index
+    @university = University.all
   end
 
   def create
-    @university = University.new(params[:name, :address, :typ])
+    @university = University.new(params.require(:university).permit(:name, :address, :typ))
     if(@university.save)
       redirect_to :action =>  :index
     else
@@ -13,9 +14,11 @@ class UniversitiesController < ApplicationController
   end
 
   def edit
+    @university = University.find params[:id]
   end
 
   def destroy
+    @univeristy = University.find params[:id]
   end
 
   def new
