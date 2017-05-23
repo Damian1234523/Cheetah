29de402ed5fa3f613b76f27100768bcdebc314c9
Damian1234523 <dpistorius@posteo.de>
2017-05-23 08:52:21 +0200

    implemented basic text controller methods

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -1,16 +1,28 @@
 class TextController < ApplicationController
   include ApplicationHelper
   before_action :authenticate_user!
+  
   def index
+    @text = Text.all
   end
 
   def create
+    @text = Text.create(params.require(:text).permit(:title, :content, :description, :exam_id))
+    @text.user_id = current_user.id
+
+    if @text.save
+      redirect_to controller: 'text', action: 'show', id: @text.id
+    else
+      render :action => :new
+    end
   end
 
   def new
+    @text = Text.new
   end
 
   def show
+    @text = Text.find(params[:id])
   end
 
   def edit
