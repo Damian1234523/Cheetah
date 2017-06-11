8bd60b3e2316fd23fb33a4ad085dfd4851683091
Damian1234523 <dpistorius@posteo.de>
2017-06-11 20:01:17 +0200

    comment partial now uses the correct route

--- config/routes.rb
+++ config/routes.rb
@@ -90,6 +90,8 @@ Rails.application.routes.draw do
 
   get 'sessions/new'
 
+  post 'comments/create'
+
   devise_for :users, controllers: { sessions: 'users/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
